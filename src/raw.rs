bf8ddd02931873df8f326bb1b9649a1b3a5524c0
h04x <liquid.doctrine@gmail.com>
2023-06-12 11:59:42 +0300

    Eq for hash

--- src/raw.rs
+++ src/raw.rs
@@ -32,7 +32,7 @@ macro_rules! missing {
 pub(crate) use malformed;
 pub(crate) use missing;
 
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub struct Hash {
     pub bytes: [u8; 20],
 }
